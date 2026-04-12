91dd575e760f0d0e92d5d2b3a22aa3e5dd1c744c
LAPTOP-ENDNOU2C\Gopan Diwan <diwangopan199@gmail.com>
2026-04-12 13:13:36 +0530

    Initial commit - setup leetcode solutions repo

---
+++ NeetCode-150/Arrays-Hashing/TwoSum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]: # pyright: ignore[reportReturnType]
+        seen = {}
+        
+        for index, value in enumerate(nums):
+            needed = target - value
+            
+            if needed in seen:
+                return [seen[needed], index]
+            
+            seen[value] = index
