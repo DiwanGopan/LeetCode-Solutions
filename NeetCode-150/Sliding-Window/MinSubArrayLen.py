b79c5e1d6e53745724fd1ae76ef763066c88c8c4
Gopan Diwan <diwangopan199@gmail.com>
2026-05-02 16:18:52 +0530

    Solve Max Subarray sum and Min Subarray Len problems

---
+++ NeetCode-150/Sliding-Window/MinSubArrayLen.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        window_sum = 0
+        min_len = float('inf')
+        for right in range(n):
+            window_sum += nums[right]
+
+            # shrink window from left while sum >= target
+            while window_sum >= target:
+                min_len = min(min_len, right - left + 1)
+                window_sum -= nums[left]
+                left += 1
+
+        if min_len == float('inf'):
+            return 0
+        else:
+            return int(min_len)
